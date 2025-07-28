1f2e6373b6ab6ec59fad642b57b4444c4bfac31b
Lenovs <edileno2223@gmail.com>
2025-07-28 12:05:39 -0300

    initial commit

---
+++ src/test/java/com/Milhas/MilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Milhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
